9b067a06a3508d49da9560c9a1f974af0e8887b2
rishabh <rishabh.gupta998@gmail.com>
2021-05-14 12:54:46 -0700

    13 may solutions

--- may/may13/min_window_substring.cpp
+++ may/may13/min_window_substring.cpp
@@ -1,6 +1,101 @@
+Problem : https://leetcode.com/problems/minimum-window-substring/
+
+// 265 and 266 TC number are failing with this solution, needs more optimisation
+
+
+// TC: O(slen*slen + tlen)
+// More to learn 
+// Very good questions
 class Solution {
 public:
+    std::string substr(string &s, int j, int i) {
+        std::string res("");
+        for (int k = j; k <=i; ++k) {
+            res += s[k];
+        }
+        return res;
+    }
     string minWindow(string s, string t) {
+        std::map<char, int> mapT; // to save the ferquency of each character in string 't'
+        std::map<char, int> mapS;
         
+        int lenS = s.size();
+        int lenT = t.size();
+        
+        int i = -1, j = -1;
+        std::string res(""); // final string
+        
+        // populating the frequency of each char in mapT
+        // t = "ABC"
+        for (auto const ch: t) {
+            mapT[ch]++;
+        }
+        // After this step
+        /*
+            A-1
+            B-1
+            C-1
+        */
+        
+        int desiredCnt = lenT; // number of character that are required
+        int currCnt = 0; // variable to maintain char count that will ultimately be equal to desiredCnt and that string can be out potential answer
+        
+        while (true) {
+            bool f1 = false;
+            bool f2 = false;
+            // acquire char by char until either the string is over or found a substring that has all the minimum chars to match the chars present in string t
+            while (i < lenS-1 && currCnt < desiredCnt) {
+                ++i;
+                auto it = mapS.find(s[i]);
+                if (it != mapS.end()) {
+                    // s[i] present in map, increase freq by 1
+                    ++mapS[s[i]];
+                } else {
+                    mapS.insert({s[i], 1});
+                }
+                
+                if (mapS[s[i]] <= mapT[s[i]]) {
+                    ++currCnt;
+                }
+                f1 = true;
+            }
+            
+            
+            // reach here hope you find atleast some string
+            // There could be some characters that can be removed until it does not affects the found substring in above while loop
+            while (j < i && currCnt == desiredCnt) {
+                // potential result 
+                ++j;
+            
+                std::string potentialResult = substr(s, j, i);// both j and i inclusive
+                if (res.length() == 0 || res.length() > potentialResult.length()) {
+                    res = potentialResult; 
+                }
+                
+                auto it = mapS.find(s[j]);
+                if (it->first == 1) {
+                    mapS.erase(it);
+                } else {
+                    --(it->second);
+                }
+                
+                if (mapS[s[j]] < mapT[s[j]]) {
+                    --currCnt;
+                }
+                    
+                f2 = true;
+            }
+            
+            if (!f1 && !f2) {
+                break;
+            }                      
+        }        
+        return res;
     }
 };
+// Very Easy Explanation: https://www.youtube.com/watch?v=e1HlptlipB0&t=1030s&ab_channel=Pepcoding
+//
+//
+//
+// Template to solve most substring problem
+// https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems
--- may/may13/next_greater_node_ll.cpp
+++ may/may13/next_greater_node_ll.cpp
@@ -1,6 +1,3 @@
-Problem Link: https://leetcode.com/problems/next-greater-node-in-linked-list/
-
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,7 +10,51 @@ Problem Link: https://leetcode.com/problems/next-greater-node-in-linked-list/
  */
 class Solution {
 public:
-    vector<int> nextLargerNodes(ListNode* head) {
+    
+    // Brute Force
+    /*
+        1. Calculate the max for each node in the list.
+        2. Put in result vector, return it.
+        3. TC: O(N^2)
+        4. SC: O(1)
+        Code:
+            curr = head;
+            vector res;
+            while (curr) {
+                nextMax = curr->next;
+                int NGE = 0;
+                while (nextMax) {
+                    NGE = std::max(NGE, nextMax->val);
+                    nextMax = nextMax->next;
+                }
+                res.push_back(NGE);
+                curr = curr->next;
+            }
 
+            return res;
+    */
+    
+    
+    // Method 1: Convert List into Array
+    // TC: O(N) -> number of total push is N  so pop will be N. 
+    vector<int> nextLargerNodes(ListNode* head) {
+       if (head == nullptr) {
+            return {};
+        }
+        //coverting to list
+        std::vector<int> res;
+        for (; head; head = head->next) res.push_back(head->val);
+        
+        std::stack<int> s;
+        for (int i = res.size() -1; i >= 0; --i) {
+            int val = res[i];
+            while (!s.empty() && s.top() <= res[i]) s.pop();
+            res[i] = s.empty() ? 0 : s.top();
+            s.push(val);
+        }
+
+        return res;
+
+        
     }
 };
